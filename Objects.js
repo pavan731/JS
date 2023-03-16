2f47f3a7994f2d0d65e620aee0fc19d6475351f4
pavan <rayabagipavan@gmail.com>
2023-03-16 17:34:29 +0530

    objects and strings

---
+++ Objects.js
@@ -0,0 +1,77 @@
+// //16/03/2023 //objects
+// // //there is no index in object alternative is keys
+
+// let employee ={
+//     name : "John",
+//     age : 30,
+//     role : "developer",
+//     salary : 30000,
+//     //nested objects
+//     skills : {
+//         FE : "html",
+//         BE : "js"
+        
+//     }
+// }
+// //console.table(employee);
+// //use table when you want to see whole data
+// // console.log(employee);  //{ name: 'John', age: 30, role: 'developer', salary: 30000 }
+
+// // employee.salary =40000 //re- initialization
+
+// // console.log(employee.salary)
+
+//  //employee.skills =["html","css","js"]     //it will add a new key
+// // console.table(employee)
+
+// // //if you want to remove a key
+// // delete employee.role        //it will remove a developer role
+// // console.table(employee)
+
+
+// //a single key can store multiple elements like array
+// //console.log(employee.skills[3])
+// // employee.skills.push("nodejs")
+// // console.log(employee)       //{
+// //                                 //  name: 'John',
+// //                                 //  age: 30,
+// //                                 //  role: 'developer',
+// //                                 //  salary: 30000,
+// //                                 //   skills: [ 'html', 'css', 'js', 'nodejs' ]
+// //                                 //  }
+// // employee.skills.splice(2,1)
+// console.table(employee);
+
+// //nested objects//  at beginning
+
+// //tables will be stored in the form of objects
+
+
+// //notes
+
+// //to connect from java to sql we need jdbc
+// //to connect frontend(js) and backend(js) is easy
+// //api = used to establish the connectivity
+
+
+// //notes for strings
+//charAt()
+//indexOf()= it returns the index of the given character that occurs first
+//syntax for indexOf()=indexOf(character,start index)
+//lastindexOf()= it returns the index value that occurs last in the given string
+//syntax remains same for both
+
+//substring == used to extract the specific part of the string
+//syntax(start index,end index(excluded))
+
+
+//objects
+
+//it is the collections of data which can be stored in keys format
+
+// endIndex= induvidual keys can be accessed using dot notation and bracket notation
+//syntax : objectName.key
+//objectName["key"]
+//
+//a single key can contain multiple values within an array or also in an object which is called as nested objects
+
